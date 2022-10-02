b29acf04b4e2d2efdcbda3b6f11045dff942db4c
aneevel <aneevel15@gmail.com>
2022-10-02 17:25:11 -0500

    Add tasks, about, nav components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { AboutComponent } from './about/about.component';
+import { TasksComponent } from './tasks/tasks.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    AboutComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
