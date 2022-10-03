55b53e18f684957d6bd58039c8e03550729dc84c
aneevel <aneevel15@gmail.com>
2022-10-03 16:51:13 -0500

    Add routing to account component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
 import { HomeComponent } from './home/home.component'
+import { AccountComponent } from './account/account.component';
 
 const routes: Routes = [
   { path: "", pathMatch: "full", redirectTo: "home" },
   { path: "home", component: HomeComponent},
-  { path: "tasks", component: TasksComponent}
+  { path: "tasks", component: TasksComponent},
+  { path: "account", component: AccountComponent}
 ];
 
 @NgModule({
