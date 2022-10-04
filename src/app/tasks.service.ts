ec021c05c79bdef4db0a1af24426f321e6cd0659
aneevel <aneevel15@gmail.com>
2022-10-04 17:51:37 -0500

    Refactor Tasks service to use task interface

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Task } from '../model/task.model';
 
 @Injectable({
   providedIn: 'root'
@@ -6,46 +7,46 @@ import { Injectable } from '@angular/core';
 export class TasksService {
 
   tasks = [
-    { id: 1, 
-      short_description: "Wash the dishes", 
-      long_description: "Dishes are piling up everywhere! Wash all the dishes to the side of the sink", 
-      priority: "red", 
-      category: "homecare", 
-      points: 15, 
-      created_date: new Date(2022, 10, 1),
-      due_date: new Date(2022, 10, 2),
-      recurring: false
-    },
-    { id: 2, 
-      short_description: "Practice guitar", 
-      long_description: "Go through your normal 1.5 hour guitar practice routine.", 
-      priority: "red", 
-      category: "hobbies", 
-      points: 50, 
-      created_date: new Date(2022, 10, 1),
-      due_date: new Date(2022, 10, 1),
-      recurring: true
-    },
-    { id: 3, 
-      short_description: "Pay credit card bill", 
-      long_description: "Make the monthly payment to credit card company", 
-      priority: "green", 
-      category: "bills", 
-      points: 25, 
-      created_date: new Date(2022, 10, 1),
-      due_date: new Date(2022, 10, 30),
-      recurring: false
-    },
+    new Task(1, 
+      "Wash the dishes", 
+      "Dishes are piling up everywhere! Wash all the dishes to the side of the sink", 
+      "red", 
+      "homecare", 
+      15, 
+      new Date(2022, 10, 1),
+      new Date(2022, 10, 2),
+      false
+    ),
+    new Task(2, 
+      "Practice guitar", 
+      "Go through your normal 1.5 hour guitar practice routine.", 
+      "red", 
+      "hobbies", 
+      50, 
+      new Date(2022, 10, 1),
+      new Date(2022, 10, 1),
+      true
+    ),
+    new Task(3, 
+      "Pay credit card bill", 
+      "Make the monthly payment to credit card company", 
+      "green", 
+      "bills", 
+      25, 
+      new Date(2022, 10, 1),
+      new Date(2022, 10, 30),
+      false
+    ),
 
   ];
 
   constructor() { }
 
-  public getTasks() : Array<{id: number, short_description: string, long_description: string, priority: string, category: string, points: number, created_date: Date, due_date: Date, recurring: boolean}> {
+  public getTasks() : Array<Task> {
     return this.tasks;
   }
 
-  public createTask(task: { id: number, short_description: string, long_description: string, priority: string, category: string, points: number, created_date: Date, due_date: Date, recurring: boolean }) {
+  public createTask(task: Task) {
     this.tasks.push(task);
   }
 }
